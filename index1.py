bb8d16efa0626733142142523694b3277543e6ed
apiss <146815568+hapissubhan@users.noreply.github.com>
2023-11-01 10:23:35 +0700

    Add files via upload

---
+++ index1.py
@@ -0,0 +1,53 @@
+# program biodata diri
+nama = "mohamad subhan alhafiz"
+nim = "0110233146"
+kelas = "TI05"
+no_telp = "0895355348142"
+alamat = "jln sampoera Rt1/Rw1"
+
+print("nama: " , nama)
+print("nim: " , nim)
+print("kelas: " , kelas)
+print("no_telpn: " , no_telp)
+print("alamat: " , alamat)
+
+# program biodata temen
+nama = "agas stiawan"
+nim = "0110233161"
+kelas = "TI05"
+no_telp = "088295747113"
+alamat = "depok lama, tapos"
+
+print("nama: " , nama)
+print("nim: " , nim)
+print("kelas: " , kelas)
+print("no_telpn: " , no_telp)
+print("alamat: " , alamat)
+
+# program menghitung berat dan tinggi badan 
+tinggi_badan = 1.6
+berat_badan = 54
+imp = berat_badan / (tinggi_badan ** 2)
+
+print("berat badan ideal kamu",(int(imp)))
+
+# program konversi celcius ke fareinheit
+celcius = 38
+imp = (celcius * 9/5) + 32
+print("imp" ,int(imp))
+
+
+# Menghitung luas permukaan tabung
+r = 15
+t = 30
+
+pi = 3.14159  
+luas_permukaan = 2 * pi * r * (r + t)
+
+print("luas permukaan =" , luas_permukaan)
+
+
+# Menghitung keliling tabung
+keliling = 2 * pi * r
+
+print("keliling =" ,keliling)
